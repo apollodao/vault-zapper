0bc46e28f00728abb70fb67d702191a8f3df2ece
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2024-03-07 13:37:35 +0100

    feat: don't allow compiling without astroport or osmosis feature is enabled

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub mod contract;
 pub mod deposit;
-mod error;
+pub mod error;
 pub mod helpers;
 pub mod lockup;
 pub mod msg;
@@ -9,3 +9,7 @@ pub mod state;
 pub mod withdraw;
 
 pub use crate::error::ContractError;
+
+// Force selecting either `astroport` or `osmosis` features, or both.
+#[cfg(not(any(feature = "astroport", feature = "osmosis")))]
+compile_error!("Must select either `astroport` or `osmosis` feature, or both");
